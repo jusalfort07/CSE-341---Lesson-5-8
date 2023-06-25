e870ff17a0eaa1f548ee0ab5eb81b165f1a56a5c
Alfred Ortega <jusalfort.07@gmail.com>
2023-06-25 10:37:51 +0800

    feat(Lesson-6): Data validation & error handling

--- app.js
+++ app.js
@@ -5,12 +5,29 @@ app.use(express.json());
 const dotenv = require('dotenv');
 dotenv.config();
 
+const bodyParser = require('body-parser');
+
 const mongodb = require('./mongodb_connection/connection');
 const port = process.env.PORT;
 
-app.use('/pets', require('./routes/pets'));
-app.use('/potential_owners', require('./routes/potential_owners'));
-app.use('/api', require('./routes/swagger'));
+app.use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/pets', require('./routes/pets'));
+app.use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/potential_owners', require('./routes/potential_owners'));
+app.use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/api', require('./routes/swagger'));
 
 mongodb.initDb((err) => {
     if (err) {
--- routes/potential_owners.js
+++ routes/potential_owners.js
@@ -1,10 +1,11 @@
 const routes = require('express').Router();
 const contactsController = require('../controllers/potential_owners');
+const { potentialOwnersValidation, validate } = require('../validation/validation.js');
 
 routes.get('/', contactsController.getPotentialOwners);
 routes.get('/:id', contactsController.getPotentialOwner);
-routes.post('/', contactsController.createPotentialOwner);
-routes.put('/:id', contactsController.updatePotentialOwner);
+routes.post('/', potentialOwnersValidation(), validate, contactsController.createPotentialOwner);
+routes.put('/:id', potentialOwnersValidation(), validate, contactsController.updatePotentialOwner);
 routes.delete('/:id', contactsController.deletePotentialOwner);
 
 module.exports = routes;
